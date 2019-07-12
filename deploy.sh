9fb26c72cf2a11f8790a5ae28561ead7ece074eb
IevgenAntonenko <antonenko.jack@gmail.com>
2019-07-12 17:52:36 +0300

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#@IgnoreInspection BashAddShebang
+docker build -t iantonenko/multi-client:latest -t iantonenko/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t iantonenko/multi-server:latest -t iantonenko/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t iantonenko/multi-worker:latest -t iantonenko/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push iantonenko/multi-client:latest
+docker push iantonenko/multi-server:latest
+docker push iantonenko/multi-worker:latest
+
+docker push iantonenko/multi-client:$SHA
+docker push iantonenko/multi-server:$SHA
+docker push iantonenko/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=iantoenko/multi-client:$SHA
+kubectl set image deployments/server-deployment server=iantoenko/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=iantoenko/multi-worker:$SHA
