aa52dc72bc15bbb176c9345a6beed81c78905c2b
IevgenAntonenko <antonenko.jack@gmail.com>
2019-07-12 18:09:17 +0300

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ docker push iantonenko/multi-server:$SHA
 docker push iantonenko/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=iantoenko/multi-client:$SHA
+kubectl set image deployments/client-deployment client=iantoenko/multi-client:$SHA
 kubectl set image deployments/server-deployment server=iantoenko/multi-server:$SHA
-kubectl set image deployments/worker-deployment server=iantoenko/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=iantoenko/multi-worker:$SHA
